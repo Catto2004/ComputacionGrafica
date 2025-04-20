254bf45e297546d5ab04be464afc51a9bde909d3
Catto2004 <Juancatto2004@hotmail.com>
2025-04-20 13:24:58 -0500

    Reorganización del proyecto y scope para cada archivo.

---
+++ Proyectos/EditorDeImagenes/Edicion.py
@@ -0,0 +1,7 @@
+# Computación Grafica: Editor de Imagenes (Basico) en Python by JDRB && JCMH
+
+# Contenido
+## Editar por canal de color (RGB).
+## Ajustar brillo y contraste.
+## Rotar imágenes.
+## Recortar imágenes.
---
+++ Proyectos/EditorDeImagenes/ui.py
@@ -0,0 +1,8 @@
+# Computación Grafica: Editor de Imagenes (Basico) en Python by JDRB && JCMH
+import pygame
+import sys
+
+# Debería hacer:
+## Dibuja botones, sliders y otros elementos de la interfaz.
+## Maneja eventos de interacción con el usuario (clicks, sliders, etc.).
+## Define clases o funciones como Boton, Slider, etc.
