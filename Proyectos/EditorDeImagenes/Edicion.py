25a2e0637ff50f00b2bc8ec0a346c77d9657c0fe
Catto2004 <Juancatto2004@hotmail.com>
2025-04-22 19:15:09 -0500

    y

--- Proyectos/EditorDeImagenes/Edicion.py
+++
@@ -1,7 +0,0 @@
-# Computación Grafica: Editor de Imagenes (Basico) en Python by JDRB && JCMH
-
-# Contenido
-## Editar por canal de color (RGB).
-## Ajustar brillo y contraste.
-## Rotar imágenes.
-## Recortar imágenes.
